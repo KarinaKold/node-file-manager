8ffd2edaa536d873ab71cc5b530575d8c5b31a0f
Karina Koldaeva <karina.mila@yandex.ru>
2023-06-25 22:57:29 +0300

    feat: get directory info

---
+++ src/navigation/directory.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import { readdir } from 'fs/promises';
+
+const path = { path: os.homedir().toString() };
+
+export async function directory() {
+    const files = await readdir(path.path, { withFileTypes: true });
+    const getFilesInfo = files
+    .map(el => ({
+        name: el.name,
+        type: el.isFile() ? 'file' : 'directory'
+    }))
+    .sort((a, b) => {
+        if (a.type === b.type) {
+            return a.name.localeCompare(b.name);
+        } else {
+            return (a.type === 'directory') ? -1 : 1;
+        }
+    });
+
+    console.table(getFilesInfo);
+}
